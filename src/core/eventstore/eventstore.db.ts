aeb0d59e82bbb57e46b4968a14ede58a9fae877d
Seba G <sg@hetacode.pl>
2021-01-07 22:44:17 +0100

    created event store db implementation

--- src/core/eventstore/eventstore.db.ts
+++ src/core/eventstore/eventstore.db.ts
@@ -1,17 +1,51 @@
 import { IEvent } from "../../commons/event-handler/event";
 import { IEventStore } from "./eventstore";
-
+import { EventData, EventStoreDBClient, FORWARDS, jsonEvent, JSONEventData, ReadRevision, ResolvedEvent, START } from "@eventstore/db-client"
 export class EventStoreDB implements IEventStore {
-    constructor(connectionString: string) {
-        
+    client?: EventStoreDBClient
+    keyPrefix = ""
+
+    constructor(connectionString: string, keyPrefix: string) {
+        this.client = new EventStoreDBClient({ endpoint: connectionString })
+        this.keyPrefix = keyPrefix
     }
 
     saveEvents = async (key: string, events: IEvent[]) => {
+        let stream = this.keyPrefix + key
+        let dbEnvents: EventData[] = []
+        for (let item of events) {
+            let e = jsonEvent({
+                type: item.type,
+                data: item as any
+            })
+            dbEnvents.push(e)
+        }
 
+        await this.client?.appendToStream(stream, dbEnvents)
     }
 
-    getEvents = async (key: string) => {
-        return [] 
+    getEvents = async (key: string): Promise<IEvent[]> => {
+        let stream = this.keyPrefix + key
+        let nextPosition: ReadRevision | undefined = START
+        let events: IEvent[] = []
+        for (; ;) {
+            let dbEvents: ResolvedEvent[] | undefined = await this.client?.readStream(stream, 100, { direction: FORWARDS, fromRevision: nextPosition })
+            if (!dbEvents || dbEvents.length == 0) {
+                break
+            }
+
+            for (let e of dbEvents) {
+                nextPosition = e.commitPosition
+                let ev = e.event?.data as IEvent
+                events.push(ev)
+            }
+
+            if (!nextPosition) {
+                break
+            }
+        }
+
+        return events
     }
 
 }
