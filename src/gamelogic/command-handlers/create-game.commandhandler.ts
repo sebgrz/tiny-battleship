6ce5148061c692f952680d2aa7d2cf26c60a321a
Seba G <sg@hetacode.pl>
2021-01-20 22:43:57 +0100

    - use game aggregator in create game command handler

--- src/gamelogic/command-handlers/create-game.commandhandler.ts
+++ src/gamelogic/command-handlers/create-game.commandhandler.ts
@@ -1,18 +1,13 @@
-import { CreateGameCommand, GameCreatedEvent, IEventHandler } from "@tb/commons";
-import { v4 as uuidv4 } from "uuid"
+import { CreateGameCommand, IEventHandler } from "@tb/commons";
 import { gameLogic } from ".."
+import { GameAggregator } from "../state/game.aggregator";
 
 export class CreateGameCommandHandler implements IEventHandler<CreateGameCommand> {
     execute = async (command: CreateGameCommand) => {
-        let gameID = uuidv4()
-        let event = new GameCreatedEvent()
-        event.creatorConnectionId = command.connectionId
-        event.creatorPlayer = command.username
-        event.creatoreBoard = command.board
-        event.gameID = gameID
-        event.name = command.gameName
+        let aggr = new GameAggregator()
+        let gameID = aggr.createGame(command)
 
-        await gameLogic.eventStore.saveEvents(gameID, [event])
+        await gameLogic.eventStore.saveEvents(gameID, aggr.getPendingEvents())
 
         // TODO:  push to service bus
     }
--- src/gamelogic/state/game.aggregator.ts
+++ src/gamelogic/state/game.aggregator.ts
@@ -1,5 +1,6 @@
-import { IEvent } from "@tb/commons";
+import { CreateGameCommand, GameCreatedEvent, IEvent } from "@tb/commons";
 import { GameState } from "./game.state";
+import { v4 as uuidv4 } from "uuid"
 
 export class GameAggregator {
     private pendingEvents: IEvent[] = []
@@ -18,4 +19,16 @@ export class GameAggregator {
     }
 
     /// Modfications
+    createGame = (command: CreateGameCommand): string => {
+        let gameID = uuidv4()
+        let event = new GameCreatedEvent()
+        event.creatorConnectionId = command.connectionId
+        event.creatorPlayer = command.username
+        event.creatoreBoard = command.board
+        event.gameID = gameID
+        event.name = command.gameName
+
+        this.pendingEvents.push(event)
+        return gameID
+    }
 }
