cf2238c52e8a05b2e8c988954fbf5bb0de0c0e45
Seba G <sg@hetacode.pl>
2021-02-04 22:42:05 +0100

    #7 - added events consumer - moved .env file to src root as a common configuration for all services

--- src/active-games-projection/lib/index.ts
+++ src/active-games-projection/lib/index.ts
@@ -1,7 +1,20 @@
-'use strict';
+import { IEventBusConsumer, RabbitMQEventBus } from "@tb/core"
+import env from "dotenv"
 
-module.exports = activeGamesProjection;
+env.config({ path: "../../.env" })
 
-function activeGamesProjection() {
-    // TODO
+export const activeGamesProjection = {
+    consumerEventBus: new RabbitMQEventBus(
+        process.env.RABBITMQ_ENDPOINT as string,
+        process.env.RABBITMQ_GAMESLOGIC_EVENTS_EXCHANGE as string,
+        process.env.RABBITMQ_ACTIVEGAMES_QUEUE as string) as IEventBusConsumer
 }
+
+let start = async () => {
+    console.log("active games projection service is running")
+    await activeGamesProjection.consumerEventBus.consume(async ev => {
+        // await activeGamesProjection.commandsManager.execute(ev)
+    })
+}
+
+start()
