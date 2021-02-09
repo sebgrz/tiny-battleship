f98ad8eebe0ffc1df8a0bdced713756aa73e6501
Seba G <sg@hetacode.pl>
2021-02-09 21:12:42 +0100

    #7 - added JoinedToGameEventHandler

--- src/active-games-projection/lib/eventhandlers/game-over.eventhandler.ts
+++ src/active-games-projection/lib/eventhandlers/game-over.eventhandler.ts
@@ -3,8 +3,11 @@ import { activeGamesProjection } from ".."
 
 export class GameOverEventHandler implements IEventHandler<GameOverEvent> {
     execute = async (event: GameOverEvent) => {
-        let toRemove = activeGamesProjection.state.games.filter(f => f.id === event.gameID)[0]
-        let index = activeGamesProjection.state.games.indexOf(toRemove)
-        activeGamesProjection.state.games.splice(index, 1)
+        let games = activeGamesProjection.state.games.filter(f => f.id === event.gameID)
+        if (games.length > 0) {
+            let toRemove = games[0]
+            let index = activeGamesProjection.state.games.indexOf(toRemove)
+            activeGamesProjection.state.games.splice(index, 1)
+        }
     }
 }
--- src/active-games-projection/lib/eventhandlers/index.ts
+++ src/active-games-projection/lib/eventhandlers/index.ts
@@ -1,7 +1,9 @@
-import { EventsManager, GameCreatedEvent, GameOverEvent } from "@tb/commons";
+import { EventsManager, GameCreatedEvent, GameOverEvent, JoinedToGameEvent } from "@tb/commons";
 import { GameCreatedEventHandler } from "./game-created.eventhandler";
 import { GameOverEventHandler } from "./game-over.eventhandler";
+import { JoinedToGameEventHandler } from "./joined-to-game.eventhandler";
 
 export const eventsManager = new EventsManager()
 eventsManager.register(GameCreatedEvent, GameCreatedEventHandler)
 eventsManager.register(GameOverEvent, GameOverEventHandler)
+eventsManager.register(JoinedToGameEvent, JoinedToGameEventHandler)
