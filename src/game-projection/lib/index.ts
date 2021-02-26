598bc40add7eb7195d20186db56f49992413b6c3
Seba G <sg@hetacode.pl>
2021-02-26 23:57:37 +0100

    #9 - added implementation of grpc service that return current game state for given player - added all the things needed to run grpc server and expose before mentioned service handler implementation

--- src/game-projection/lib/index.ts
+++ src/game-projection/lib/index.ts
@@ -2,6 +2,8 @@ import { IEventBusConsumer, RabbitMQEventBus } from "@tb/core"
 import { eventsManager } from "./eventhandlers"
 import env from 'dotenv'
 import { GamesState } from "./state/games-state"
+import grpc from "grpc"
+import grpc_service_handlers from "./grpc"
 
 env.config({ path: "../.env" })
 
@@ -17,6 +19,21 @@ export const gameProjection = {
 let start = async () => {
     console.log("game projection service is running")
 
+    let srv = new grpc.Server()
+    srv.addService(grpc_service_handlers.service, grpc_service_handlers.handler)
+    srv.bindAsync(`0.0.0.0:${process.env.GAME_PROJECTION_SVC_GRPC_PORT}`,
+        grpc.ServerCredentials.createInsecure(),
+        (e, p) => {
+            if (e) {
+                console.error(e)
+                return
+            }
+
+            console.log("game projection service is listening on port " + p)
+        })
+    srv.start()
+
+    console.log("start events bus consumer...")
     await gameProjection.consumerEventBus.consume(async ev => {
         await gameProjection.eventsManager.execute(ev)
     })
